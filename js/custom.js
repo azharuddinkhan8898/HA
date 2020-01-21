b0b1330522eb15b938197a91119d4f1a249360c0
Ayushi <ayushi@M16MBHDSXG8WP.local>
2020-01-21 21:11:18 +0530

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,44 @@
 $(function(){
     $('.slick-slider-wrapper').slick({
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
       });
       $('.about-team-wrapper .row').slick({
         slidesToShow: 5,
         slidesToScroll: 1
       });
+      $('.top-banner-slider').slick({
+        dots: true,
+      });
+
       
-      $(".fancybox").fancybox();
+      
       $(".fancybox.ifrane").fancybox({type:'iframe'});
 
       var Pagination = {
